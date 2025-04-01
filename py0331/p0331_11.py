50558895818a7e4794e1a8a8e8047d1ab2ce0c34
yukarina11 <yukarina11@korea.ac.kr>
2025-04-01 18:31:50 +0900

    표적(최적화만 남음)

--- py0331/p0331_11.py
+++ py0331/p0331_11.py
@@ -5,8 +5,58 @@ a_list = [[0]*5 for i in range(5)]
 for i in range(5):
     for j in range(5):
         a_list[i][j]=sample_list[5*i+j]
-print(a_list)
+count = 0
+discount = 0
+bingo = 0
 while True:
+    for i in range(5): #세로
+        for j in range(5):
+            if a_list[count][i] == "$": discount +=1
+            if a_list[count][i] == "X" or a_list[count][i] == "$":
+                count +=1
+            if discount == 5: count -=1
+            if count == 5:
+                for k in range(5):
+                    a_list[k][i] = "$"
+                bingo +=1
+                break
+        discount = 0
+        count = 0
+    for i in range(5): # 가로
+        for j in range(5):
+            if a_list[i][count] == "$" : discount +=1
+            if a_list[i][count] == "X" or a_list[i][count] == "$":
+                count +=1
+            if discount == 5: count -=1
+            if count == 5:
+                for k in range(5):
+                    a_list[i][k] = "$"
+                bingo +=1
+                break
+        discount = 0
+        count = 0
+    for j in range(5): # 왼대각선
+        if a_list[count][count] == "$" : discount +=1
+        if a_list[count][count] == "X" or a_list[count][count] == "$":
+            count +=1
+        if discount == 5: count -=1
+        if count == 5:
+            for k in range(5): a_list[k][k] = "$"
+            bingo +=1
+            break
+    discount = 0
+    count = 0
+    for j in range(5): #오른대각선
+        if a_list[count][4-count] == "$": discount +=1
+        if a_list[count][4-count] == "X" or a_list[count][4-count] == "$":
+            count +=1
+        if discount == 5: count -=1
+        if count == 5:
+            for k in range(5): a_list[k][4-k] = "$"
+            bingo +=1
+            break
+    discount = 0
+    count = 0
     print(" |",end=" ")
     for i in range(5):
         print(f"{i}",end="\t")
@@ -17,11 +67,17 @@ while True:
         for j in range(5):
             print(f"{a_list[i][j]}",end="\t")
         print()
-    num1 = int(input("X좌표를 입력하세요.>>"))
-    if num1>4 or num1 <0: num1 = int(input("X좌표를 다시 입력하세요.>>"))
-    num2 = int(input("Y좌표를 입력하세요.>>"))
-    if num2>4 or num2 <0: num2 = int(input("Y좌표를 다시 입력하세요.>>"))
-    a_list[num2][num1]= "X"
-    
-            
-    
+    if bingo==3:
+        print("!!!!!! 빙고 완성 축하드립니다. !!!!!!")
+        break
+    print()
+    num = int(input("숫자를 입력하세요.(1~25)>> "))
+    while num > 25 or num < 1 : num = int(input("다시 숫자를 입력하세요.(1~25)>> "))
+    stop = 0
+    for i in range(5):
+        for j in range(5):
+            if a_list[i][j] == num:
+                a_list[i][j] = "X"
+                stop = 1
+                break
+        if stop == 1: break
